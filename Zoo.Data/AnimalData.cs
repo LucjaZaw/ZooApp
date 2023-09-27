302eb11d579674ec6854180a6efca4d4357f82aa
zawia <zawia@DESKTOP-K3G1MM9>
2023-09-27 13:25:56 +0200

    Add project files.

---
+++ Zoo.Data/AnimalData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using Zoo.Common;
+
+namespace Zoo.Data
+{
+	public struct AnimalData
+	{
+	Animal[] Animals { get; } = new[] {
+		new Animal("Lion", true),
+		new Animal("Giraffe", false),
+		new Animal("Quakka", false),
+		new Animal("Tiger", true),
+		};
+
+        public AnimalData()
+        {
+            
+        }
+
+		public IEnumerable<Animal> GetAnimals(bool isPredator) { 
+			foreach (var animal in Animals)
+				if(animal.Predator.Equals(isPredator))
+				yield return animal;
+		}
+    }
+		
+}
